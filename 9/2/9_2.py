89fff40d5817a9a0553be50ba98d5425a4cf8a81
cbiers <chris.biersbach@gmail.com>
2025-12-09 09:09:25 +0100

    Day 9 done

--- 9/2/9_2.py
+++ 9/2/9_2.py
@@ -1,15 +1,20 @@
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
 example = False
-with open("9/1/ex.txt" if example else "9/1/in.txt") as file: points = [list(map(int, line.strip().split(","))) for line in file]
+with open("ex.txt" if example else "in.txt") as file: points = [Point(p[0], p[1]) for p in [list(map(int, line.strip().split(","))) for line in file]]
+poly = Polygon(points)
 
 best = 0
 
-for i in range(len(points)):
-    for j in range(i + 1, len(points)):
-        xmin = min(points[i][0], points[j][0])
-        xmax = max(points[i][0], points[j][0])
-        ymin = min(points[i][1], points[j][1])
-        ymax = max(points[i][1], points[j][1])
+for pa in points:
+    for pb in points:
+        xmin = min(pa.x, pb.x)
+        xmax = max(pa.x, pb.x)
+        ymin = min(pa.y, pb.y)
+        ymax = max(pa.y, pb.y)
+        poly2 = Polygon([Point(xmin, ymin), Point(xmin, ymax), Point(xmax, ymax), Point(xmax, ymin)])
         rect_size = (xmax - xmin + 1) * (ymax - ymin + 1)
-        if rect_size > best:
+        if poly.contains(poly2) and rect_size > best:
             best = rect_size
 print(best)
