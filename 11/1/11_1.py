06f81d468b66a05f065dda7a2004aedf48260dbf
cbiers <chris.biersbach@gmail.com>
2025-12-11 10:52:03 +0100

    Renamed functions for more conciseness

--- 11/1/11_1.py
+++ 11/1/11_1.py
@@ -1,12 +1,12 @@
 example = False
 with open("11/1/ex.txt" if example else "11/1/in.txt") as file: devices = {name: outputs.split() for name, outputs in (line.strip().split(": ") for line in file)}
 
-def number_of_paths_to_out(device):
-    if device == "out":
+def count_paths(device, output_device):
+    if device == output_device:
         return 1
     total_paths = 0
     for output in devices[device]:
-        total_paths += number_of_paths_to_out(output)
+        total_paths += count_paths(output, output_device)
     return total_paths
 
-print(number_of_paths_to_out("you"))
+print(count_paths("you", "out"))
--- 11/2/11_2.py
+++ 11/2/11_2.py
@@ -4,14 +4,14 @@ example = False
 with open("11/2/ex.txt" if example else "11/1/in.txt") as file: devices = {name: outputs.split() for name, outputs in (line.strip().split(": ") for line in file)}
 
 @cache
-def find_paths_to(device, output_device):
+def count_paths(device, output_device):
     if output_device != "out" and device == "out":
         return 0
     elif device == output_device:
         return 1
     paths = 0
     for output in devices[device]:
-        paths += find_paths_to(output, output_device)
+        paths += count_paths(output, output_device)
     return paths
 
-print(find_paths_to("svr", "fft") * find_paths_to("fft", "dac") * find_paths_to("dac", "out"))
+print(count_paths("svr", "fft") * count_paths("fft", "dac") * count_paths("dac", "out"))
